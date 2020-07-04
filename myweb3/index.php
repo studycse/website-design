d8790232becb56c61b742edb7a08ef825158a4ea
Samia Rahmna <67817437+studycse@users.noreply.github.com>
2020-07-04 13:08:02 +0600

    Add files via upload

---
+++ myweb3/index.php
@@ -0,0 +1,589 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css"/>
+  <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+ 
+</head>
+<body>
+
+	<div class="header" id="topheader" >
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+			<div class="container text-uppercase p-2 " >
+
+  <a class="navbar-brand" href="#">Online Education</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto text-uppercase">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#servicediv">Service</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#pricingdiv">Price</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#newsletterid">About</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#contactid">Contact</a>
+      </li>
+     
+      </ul>
+   
+  </div>
+  </div>
+</nav>
+
+<section class="header-section">
+	<div class="center-div ">
+		<h1 class="font-wight-bold">Start Online Education </h1>
+		<p>Online Education Elearning and Online Eduction institute</p>
+		<div class="header-buttons">
+			<a href="#newsletterid">Aboutus</a>
+			<a href="#newsletterid">Contact</a>
+			
+		</div>
+		
+	</div>
+</section>
+		</div>
+ <!--*******header parts end*******-->
+
+ <!--******* 3 extra headerpart*****-->
+               <section class="header-extradiv">
+               	<div class="container">
+               		<div class="row">
+               			<div class="extra-div col-lg-4 col-md-4 col-12">
+               				<a href="#"><i class=" fa-3x fa fa-desktop" aria-hidden="true"></i></a>
+               				<h2>Courses Online</h2>
+               				<p>A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose</p>
+               			</div>
+               			<div class=" extra-div col-lg-4 col-md-4 col-12">
+               				<a href="#"><i class="fa-3x fa fa-trophy" aria-hidden="true"></i></a>
+               				<h2>Certified Teachers</h2>
+               				<p>A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose</p>
+               			</div>
+
+               			<div class="extra-div col-lg-4 col-md-4 col-12">
+               				<a href="#"><i class="fa-3x fa fa-magic" aria-hidden="true"></i></a>
+               				<h2>Apply now</h2>
+               				<p>A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose</p>
+               			</div>
+               			
+               		</div>
+               		
+               	</div>
+                        
+                  </section>
+
+<!--******* 3 extra headerpartend*****-->
+
+<!--******* offer start*****-->
+
+<section class="Serviceoffers" id="servicediv">
+	<div class="container headings text-center">
+		<h1 class="text-center font-wight-bold">What do we Offers</h1>
+		<p class="text-center">A paragraph consists of one or more sentences.</p>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-12 col-10 offset-1 offset-lg-0 ">
+					
+					<div class="names my-3">
+						<h1>Html</h1>
+						<div class="progress w-75">
+						<div class="progress-bar progress-bar-striped progress-bar-animated" style="width:100%">100%</div>
+
+						</div>
+
+						<div class="names my-3">
+						<h1>Css</h1>
+						<div class="progress w-75">
+						<div class="progress-bar progress-bar-striped progress-bar-animated bg-success" style="width:100%">100%</div>
+
+						</div>
+
+						<div class="names my-3">
+						<h1>Javascript</h1>
+						<div class="progress w-75">
+						<div class="progress-bar progress-bar-striped progress-bar-animated bg-info" style="width:100%">100%</div>
+
+						</div>
+
+						<div class="names my-3">
+						<h1>jquery</h1>
+						<div class="progress w-75">
+						<div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" style="width:100%">100%</div>
+					</div>
+
+						<div class="names my-3">
+						<h1>Nodejs</h1>
+						<div class="progress w-75">
+						<div class="progress-bar progress-bar-striped progress-bar-animated  bg-danger" style="width:100%">100%</div>
+						</div>
+					</div>
+			
+			</div>
+
+
+			</div>
+			
+		</div>
+
+</div>
+ </div>
+  <div class="col-lg-6 col-md-12 col-12 servicediv">
+				<div class="row">
+					<div class="col-lg-2 col-2 service-icons">
+						<i class=" fa-3x fa fa-desktop" aria-hidden="true"></i>
+					</div>
+							<div class="col-lg-10 col-10">
+								<h2>Website Development</h2>
+								<p>A paragraph consists of one or more sentences</p>
+								
+							</div>				
+							</div>
+
+							<div class="row">
+					<div class="col-lg-2 col-2 service-icons">
+						<i class=" fa-3x fa fa-wifi" aria-hidden="true"></i>
+					</div>
+							<div class="col-lg-10 col-10">
+								<h2>Digital Marketing</h2>
+								<p>A paragraph consists of one or more sentences</p>
+								
+							</div>				
+							</div>
+
+							<div class="row">
+					<div class="col-lg-2 col-2 service-icons">
+						<i class=" fa-3x fa fa-phone" aria-hidden="true"></i>
+					</div>
+							<div class="col-lg-10 col-10">
+								<h2>Support 24/7</h2>
+								<p>A paragraph consists of one or more sentences</p>
+								
+							</div>				
+							</div>
+				
+				
+			</div>
+  </div>
+   </div>
+
+
+	
+</section>
+
+
+
+<!--******* offer start end*****-->
+
+<!--******* project start *****-->
+
+<section class="project-work" >
+	<div class="container headings text-center">
+		<p class="text-center font-wight-bold">More then 2,000 website created</p>
+		
+		</div>
+	
+		<div class="container d-flex justify-content-around aling-items-center text-center  ">
+			<div>
+				<h1 class="count">1500</h1>
+				<p>CMS Installation</p>
+			</div>
+
+			<div>
+				<h1 class="count">2500</h1>
+				<p>Awards On</p>
+			</div>
+
+			<div>
+				<h1 class="count">700</h1>
+				<p>Happy Clients</p>
+			</div>
+
+			<div>
+				<h1 class="count">500</h1>
+				<p>Working on</p>
+			</div>
+			
+		</div>
+</section>
+
+
+
+<!--******* project start end*****-->
+
+<!--******* our pricing start*****-->
+
+<section class="pricing" id="pricingdiv">
+	<div class="container headings text-center">
+		<h1 class="text-center font-wight-bold text-white">OUR BEST PRICE</h1>
+	</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-12">
+					<div class="card text-center">
+					  <div class="card-header">BASIC</div>
+					  <div class="card-body">
+					  	<li>$<span class="money">20</span>/Website </li>
+					  	<li>Responsive Website</li>
+					  	<li>Domain name free</li>
+					  	<li>Mobile friendly</li>
+					  	<li>Webmail Support</li>
+					  	<li>Customer support 24/7</li>
+					  </div>
+					  <div class="card-footer"><a href="#">Purchase</a></div>
+					  
+					</div>
+
+					</div>
+				
+					<div class="col-lg-4 col-12">
+					<div class="card text-center">
+					  <div class="card-header">STANDART</div>
+					  <div class="card-body">
+					  	<li>$<span class="money">50</span>/Website </li>
+					  	<li>Responsive Website</li>
+					  	<li>Domain name free</li>
+					  	<li>Mobile friendly</li>
+					  	<li>Webmail Support</li>
+					  	<li>Customer support 24/7</li>
+					  </div>
+					  <div class="card-footer"><a href="#">Purchase</a></div>
+					  
+					</div>
+
+					</div>
+
+					<div class="col-lg-4 col-12">
+					<div class="card text-center">
+					  <div class="card-header">UNLIMITED</div>
+					  <div class="card-body">
+					  	<li>$<span class="money">80</span>/Website </li>
+					  	<li>Responsive Website</li>
+					  	<li>Domain name free</li>
+					  	<li>Mobile friendly</li>
+					  	<li>Webmail Support</li>
+					  	<li>Customer support 24/7</li>
+					  </div>
+					  <div class="card-footer"><a href="#">Purchase</a></div>
+					  <
+					</div>
+
+					</div>
+
+			</div>
+			
+		</div>
+		</section>
+
+
+
+<!--******* our pricing end*****-->
+
+<!--******* customers feed back*****-->
+<section class="happyclient" >
+	<div class="container headings text-center">
+		<h1 class="text-center font-wight-bold ">OUR HAPPY CLIENTS</h1>
+		<p class="text-capitalize pt-1">Our Happy Client</p>
+	</div>
+
+	<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+
+  <!-- The slideshow -->
+  <div class="carousel-inner container">
+    <div class="carousel-item active">
+
+      <div class="row">
+      	<div class="col-lg-4 col-md-4 col-12 ">
+      		<div class="box">
+      			<a href="#"><img src="image/p2.jpg" class="img-fluidi img-thumbnail"></a>
+      			<p class="m-4">A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.</p>
+      			<h1>Samia Rahman</h1>
+      			<h2>Web Developer</h2>
+      		</div>
+      	</div>	
+    
+            <div class="col-lg-4 col-md-4 col-12 ">
+      		<div class="box">
+      			<a href="#"><img src="image/p2.jpg" class="img-fluidi img-thumbnail"></a>
+      			<p class="m-4">A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.</p>
+      			<h1>Samia Rahman</h1>
+      			<h2>Web Developer</h2>
+      		</div>
+      	</div>	
+      
+      	<div class="col-lg-4 col-md-4 col-12 ">
+      		<div class="box">
+      			<a href="#"><img src="image/p2.jpg" class="img-fluidi img-thumbnail"></a>
+      			<p class="m-4">A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.</p>
+      			<h1>Samia Rahman</h1>
+      			<h2>Web Developer</h2>
+      		</div>
+      	</div>	
+     </div>
+     
+    </div>
+    <div class="carousel-item">
+      
+      <div class="row">
+      	<div class="col-lg-4 col-md-4 col-12 ">
+      		<div class="box">
+      			<a href="#"><img src="image/p2.jpg" class="img-fluidi img-thumbnail"></a>
+      			<p class="m-4">A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.</p>
+      			<h1>Samia Rahman</h1>
+      			<h2>Web Developer</h2>
+      		</div>
+      	</div>	
+      
+      	<div class="col-lg-4 col-md-4 col-12 ">
+      		<div class="box">
+      			<a href="#"><img src="image/p2.jpg" class="img-fluidi img-thumbnail"></a>
+      			<p class="m-4">A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.</p>
+      			<h1>Samia Rahman</h1>
+      			<h2>Web Developer</h2>
+      		</div>
+      	</div>	
+     
+
+      
+      	<div class="col-lg-4 col-md-4 col-12 ">
+      		<div class="box">
+      			<a href="#"><img src="image/p2.jpg" class="img-fluidi img-thumbnail"></a>
+      			<p class="m-4">A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.</p>
+      			<h1>Samia Rahman</h1>
+      			<h2>Web Developer</h2>
+      		</div>
+      	</div>	
+      </div>
+    </div>
+    <div class="carousel-item">
+      
+      <div class="row">
+      	<div class="col-lg-4 col-md-4 col-12 ">
+      		<div class="box">
+      			<a href="#"><img src="image/p2.jpg" class="img-fluidi img-thumbnail"></a>
+      			<p class="m-4">A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.</p>
+      			<h1>Samia Rahman</h1>
+      			<h2>Web Developer</h2>
+      		</div>
+      	</div>	
+      
+
+  
+      	<div class="col-lg-4 col-md-4 col-12 ">
+      		<div class="box">
+      			<a href="#"><img src="image/p2.jpg" class="img-fluidi img-thumbnail"></a>
+      			<p class="m-4">A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.</p>
+      			<h1>Samia Rahman</h1>
+      			<h2>Web Developer</h2>
+      		</div>
+      	</div>	
+     
+
+     
+      	<div class="col-lg-4 col-md-4 col-12 ">
+      		<div class="box">
+      			<a href="#"><img src="image/p2.jpg" class="img-fluidi img-thumbnail"></a>
+      			<p class="m-4">A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.</p>
+      			<h1>Samia Rahman</h1>
+      			<h2>Web Developer</h2>
+      		</div>
+      	</div>	
+      </div>
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+
+</div>
+</section>
+<!--******* customers feed back*****-->
+
+<!--*******contact from*****-->
+<section class="contactus" id="contactid" >
+	<div class="container headings text-center">
+		<h1 class="text-center font-wight-bold ">CONTACT US</h1>
+		<p class="text-capitalize pt-1">We are here to help and answer any question you might have. We look forward to hearing from you</p>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-md-8 col-10 offset-lg-2 offset-md-2 offset-1">
+
+				<form action="/action_page.php">
+
+				  <div class="form-group">
+				    
+				    <input type="text" class="form-control" placeholder="Enter name" id="username"
+				     required autocomplete="off">
+				  </div>
+
+				  <div class="form-group">
+				    
+				    <input type="email" class="form-control" placeholder="Enter email" id="email"
+				    required autocomplete="off">
+				  </div>
+
+				  <div class="form-group">
+				    
+				    <input type="number" class="form-control" placeholder="Enter number" id="mobile"
+				    required autocomplete="off">
+				  </div>
+
+			<div class="form-group">
+			  
+			  <textarea class="form-control" placeholder="Enter comment" rows="4" id="comment"></textarea>
+			</div>
+				 
+
+				  <div class="d-flex justify-content-center form-button">				 
+				   <button type="submit" class="btn btn-primary">Submit</button>
+				</form>
+				</div>
+			</div>
+			
+		</div>
+		
+	</div>
+
+</section>
+
+<!--*******contact from*****-->
+
+<!--*******news letter start*****-->
+
+<section class="newsletter" id="newsletterid" >
+	<div class="container headings text-center">
+		<h1 class="text-center font-wight-bold ">SUBSCRIBE TO OUR NEWSLETTER</h1>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2 col-12">
+
+				 <div class="input-group mb-3">
+			    <input type="text" class="form-control news-input" placeholder="Your Email">
+			    <div class="input-group-append">
+			      <span class="input-group-text">Subscribe</span>
+			    </div>
+			  </div>
+					
+				</div>
+				
+			</div>
+			
+		</div>
+
+	</section>	
+
+<!--*******news letter end*****-->
+
+<!--*******footer*****-->
+<footer class="footersection" id="footerdiv">
+	      
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-6 col-12 footer-div">
+					<h3>Web Development</h3>
+					<p>A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language.</p>
+					
+				</div>
+
+				<div class="col-lg-4 col-md-6 col-12 footer-div text-center">
+					<h3>Navigation Link</h3>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Service</a></li>
+					<li><a href="#">Price</a></li>
+					<li><a href="#">About</a></li>
+					
+				</div>
+
+				<div class="col-lg-4 col-md-12 col-12 footer-div">
+					<h3>News Letter</h3>
+					<p>A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. </p>
+
+						<div class="container newsletter-main">
+						<div class="row">
+						<div class="col-lg-12 col-12">
+
+						 <div class="input-group mb-3">
+					    <input type="text" class="form-control news-input" placeholder="Your Email">
+					    <div class="input-group-append">
+					      <span class="input-group-text">Subscribe</span>
+					    </div>
+					  </div>
+					
+				</div>
+				
+			</div>
+			
+		</div>
+					
+				</div>
+				
+			</div>
+
+			<div class="mt-5 text-center">
+			<p>This template is made by SamiaRahman @2020</p>
+				
+			</div>
+			
+		</div>
+
+</footer>
+
+<!--*******footer*****-->
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js" integrity="sha256-jDnOKIOq2KNsQZTcBTEnsp76FnfMEttF6AV2DF2fFNE=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.min.js" integrity="sha256-JtQPj/3xub8oapVMaIijPNoM0DHoAtgh/gwFYuN5rik=" crossorigin="anonymous"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+<script type="text/javascript">
+	$('.count').counterUp({
+		
+		delay:10,
+		time:3000
+	});
+
+	
+</script>
+
+</body>
+</html>
