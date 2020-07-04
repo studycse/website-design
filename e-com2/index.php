ed3f0c60c4e471f05dae8d93ffcb6b34067b56bc
Samia Rahmna <67817437+studycse@users.noreply.github.com>
2020-07-04 13:06:46 +0600

    Add files via upload

---
+++ e-com2/index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css?v=<?php echo time(); ?>">
+
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+<section>
+	<div class="header">
+<!---logo---->
+		<div class="logo">
+		<h1>SAMIA</h1>
+		</div>
+<!--link----->
+<div class="link">
+	<ul>
+		<li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i> 24/7 Customer Care |</a></li>
+		<li><a href="#">Sign Up |</a></li>
+		<li><a href="#"><i class="fa fa-user" aria-hidden="true"></i> Login</a></li>
+		
+	</ul>
+	
+</div>
+
+<!--- search button---->
+<div class="search">
+	<form method="post">
+		<input type="text" name="form-control" >
+		<input type="submit" value="Search">
+		<button><i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart</button>
+		</form>
+
+	</div>
+
+</div>
+
+</section>
+<!---navbar-------->
+<div class="menu-bar">
+<ul>
+		<li class="active"><a href="#"><i class="fa fa-user"></i> About Us</a>
+			<div class="sub-menu-1">
+				<ul>
+				<li><a href="#">Mission</a></li>
+				<li><a href="#">Vision</a></li>
+				<li><a href="#">Team</a></li>
+				<li><a href="#">Organizer</a></li>
+				</ul>
+			</div>
+
+		</li>
+		<li><a href="#"><i class="fa fa-clone"></i> Services</a>
+
+			<div class="sub-menu-1">
+				<ul>
+				<li><a href="#">Mission</a></li>
+				<li><a href="#">Vision</a></li>
+				<li><a href="#">Team</a></li>
+				<li><a href="#">Organizer</a></li>
+				</ul>
+			</div>
+
+
+
+
+		</li>
+		<li><a href="#"><i class="fa fa-users"></i> Clients</a></li>
+		<li><a href="#"><i class="fa fa-angellist"></i> Investers</a></li>
+		<li><a href="#"><i class="fa fa-inr"></i> Pricing</a></li>
+		<li><a href="#"><i class="fa fa-edit"></i> Training</a></li>
+		<li><a href="#"><i class="fa fa-phone"></i> Contact</a></li>
+	</ul>
+	
+
+</div>	
+
+
+
+
+<!---body layer -------->
+<section>
+
+	<div class="bodyleft">
+		<h2>Deals of the Day</h2>
+		<div class="slider">
+			<img src="image/c4.jpg">
+			
+		</div>
+<!--product list--->
+		<ul>
+			<h3>Shoes</h3>
+			<li>
+				<a href="#"><h4>Casual Shoes</h4>
+					<img src="image/p3.jpg">
+				</a>
+			</li>
+
+			<li>
+				<a href="#"><h4>Casual Shoes</h4>
+					<img src="image/p3.jpg">
+				</a>
+			</li>
+
+			<li>
+				<a href="#"><h4>Casual Shoes</h4>
+					<img src="image/p3.jpg">
+				</a>
+			</li>
+		</ul><br clear="all" />
+		
+<!----2nd Product------>
+		<ul>
+			<h3>Mobile Phone</h3>
+			<li>
+				<a href="#"><h4>Casual Shoes</h4>
+					<img src="image/p3.jpg">
+				</a>
+			</li>
+
+			<li>
+				<a href="#"><h4>Casual Shoes</h4>
+					<img src="image/p3.jpg">
+				</a>
+			</li>
+
+			<li>
+				<a href="#"><h4>Casual Shoes</h4>
+					<img src="image/p3.jpg">
+				</a>
+			</li>
+		</ul>
+			
+	</div>
+
+	<div class="bodyright">
+		<h3>Greate Deals</h3>
+		<ul>
+			<li><a href="#"><img src="image/001.jpg"></a></li>
+			<li><a href="#"><img src="image/001.jpg"></a></li>
+			<li><a href="#"><img src="image/001.jpg"></a></li>
+			<li><a href="#"><img src="image/001.jpg"></a></li>
+		</ul>
+		
+	</div>
+
+</section><br clear="all" />
+<section>
+	<div class="footer">
+		<h4>Made by Samia Rahman 2020</h4>
+		
+	</div>
+
+
+
+
+</section>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ e-com2/index2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css?v=<?php echo time(); ?>">
+
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="menu-bar">
+<ul>
+		<li class="active"><a href="#"><i class="fa fa-user"></i> About Us</a>
+			<div class="sub-menu-1">
+				<ul>
+				<li><a href="#">Mission</a></li>
+				<li><a href="#">Mission</a></li>
+				<li><a href="#">Mission</a></li>
+				<li><a href="#">Mission</a></li>
+				</ul>
+			</div>
+
+
+
+
+		</li>
+		<li><a href="#"><i class="fa fa-clone"></i> Services</a></li>
+		<li><a href="#"><i class="fa fa-users"></i> Clients</a></li>
+		<li><a href="#"><i class="fa fa-angellist"></i> Investers</a></li>
+		<li><a href="#"><i class="fa fa-inr"></i> Pricing</a></li>
+		<li><a href="#"><i class="fa fa-edit"></i> Training</a></li>
+		<li><a href="#"><i class="fa fa-edit"></i> Contact</a></li>
+	</ul>
+	
+
+</div>	
+
+
+</body>
+</html>
