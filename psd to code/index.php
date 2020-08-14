d2533786c68398adec54261eccb156b1315b7f3a
Samia Rahmna <67817437+studycse@users.noreply.github.com>
2020-08-14 11:46:04 +0600

    Add files via upload

---
+++ psd to code/index.php
@@ -0,0 +1,450 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+
+<!-- font awesome -->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/js/all.min.js" integrity="sha512-I2eVORN4ZTPhAiP2pz5d6wQ3ScD/h7ORW4WoCsl3/neI8RSrRnBl2kW3myjTUN+J0Ml4fRT31ZLz08gQb/eo1g==" crossorigin="anonymous"></script>
+
+  <link rel="stylesheet" type="text/css" href="./css/style.css?v=<?php echo time(); ?> ">
+
+    <title>Psd to html</title>
+  </head>
+
+
+
+  <body>
+
+    <!--navbar------>
+   <header class="container-fluid nav_style mx-auto"> <!-- mx-auto content sob center a asbe--->
+    <div class="row">
+      <div class="col-md-10 col-11 mx-auto">
+
+        <nav class="navbar navbar-expand-lg navbar-light ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">SAMIA</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+      <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+
+         <li class="nav-item">
+          <a class="nav-link" href="#">Service</a>
+        </li>
+
+         <li class="nav-item">
+          <a class="nav-link" href="#">Portfolio</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        
+       
+      </ul>
+     
+    </div>
+  </div>
+</nav>
+        
+      </div>
+      
+    </div>
+     
+ </header>
+
+ <!----main header part--------->
+ <!-------left center content--------->
+ <div class="container-fluid">
+  <div class="row">
+    <div class="col-11 col-md-10 mx-auto">
+
+      <div class="row">
+       <div class="col-md-6 col-11 order-2 order-md-1">
+        <p class="header-text">Hi Dear <br>
+        The Standard Chunk of <span class="span-header-text">Lorem Ipsum</span> <br>
+        used since the <span class="span-header-text">1500s </span>
+          </p>
+
+          <p class="header-text2">Cicero are also reproduce<br>
+          <span class="span-header-text">=STENAME</span>
+        </p>
+
+<!-------button--------->
+<div class="d-flex justify-content-around align-item-center ">
+        <div class="">
+          <button class="hire-me"><i class="fas fa-check"></i> Hire Me</button>
+        </div>
+
+          <div class="">
+          <button class="about-me"><i class="fas fa-user"></i> About Me</button>
+        </div>
+      </div>
+<!-------Email part--------->
+      <div class="text-center header-mail">
+        <p class="font-wight-bold header-email-pera">Email
+          <p>test@gmail</p>
+        </p>
+        
+      </div>
+        
+      </div>
+
+<!-------Right center content--------->
+
+
+       <div class="col-md-6 col-11 order-md-2 order-1">
+        <div class="float-right">
+          <figure class="header-image">
+            <img src="image/p1.png" class="img-fluid" alt="main img">
+          </figure>
+        </div>
+        </div>
+        
+      </div>
+
+      
+    </div>
+     
+  </div>
+   
+ </div>
+
+
+ <!----main header part--------->
+
+  <!----about me part--------->
+
+   <div class="container-fluid about-sec bg-light" id="about">
+  <div class="row">
+    <div class="col-11 col-md-10 mx-auto">
+      <div class="main-title">
+        <h1>About Me</h1>
+        
+      </div>
+
+      <div class="row">
+       <div class="col-md-5 col-11">
+        <figure class="header-image">
+            <img src="image/p4.png" class="img-fluid" alt="about img">
+          </figure>
+
+       </div>
+
+       <div class="col-md-7 col-11 about-text d-flex justify-content-center align-items-start flex-column ">
+        <p>Hello</p>
+        <h1>I am a passionate Designer</h1>
+        <p>I have 3 year Experience. I can help you grow your buisness by something different and new for your buisness. I have done a lot of buisness and i can have help you a lot</p>
+        
+       </div>
+     </div>
+   </div>
+ </div>
+</div>
+
+   <!----about me part--------->
+
+    <!----expert part--------->
+
+   <div class="container-fluid expert-sec " id="about">
+  <div class="row">
+    <div class="col-11 col-md-10 mx-auto">
+      <div class="main-title">
+        <h1>Work Expertise</h1>
+        <p class="text-center">obviously I'm Graphic designer <br/> Experienced with all stages of the design cycle with every project</p>
+        
+      </div>
+
+      <div class="row">
+       
+       <div class=" common-div col-md-7 col-11 about-text d-flex justify-content-center align-items-start flex-column ">
+
+       <div class="w-100">
+        <h2 class="expert-h1">Photoshop</h2>
+        <div class="progress">
+         <div class="progress-bar pb-1" role="progressbar" >
+    
+         </div>
+        </div>
+
+        </div>
+
+         <div class="w-100 my-5">
+        <h2 class="expert-h2">Branding</h2>
+        <div class="progress">
+         <div class="progress-bar pb-2" role="progressbar" >
+    
+         </div>
+        </div>
+
+        </div>
+
+         <div class="w-100">
+        <h2 class="expert-h3">Ilustrator</h2>
+        <div class="progress">
+         <div class="progress-bar pb-3" role="progressbar" >
+    
+         </div>
+        </div>
+
+        </div>
+        
+       </div>
+
+       <div class="col-md-5 col-12 common-div">
+        <figure class="float-md-right text-center">
+            <img src="image/p4.png" class="img-fluid" alt="about img">
+          </figure>
+
+       </div>
+
+     </div>
+   </div>
+ </div>
+</div>
+
+     <!----expart me part--------->
+
+
+     <!----our services part--------->
+       <div class="container-fluid ourservices bg-light" id="about">
+
+  <div class="row">
+    <div class="col-11 col-md-10 mx-auto">
+      <div class="main-title">
+        <h1>Our Services</h1>
+        <p>We Provide High <b>Quality-creative Services</b></p>
+        
+      </div>
+
+      <div class="row justify-content-around mt-5 gy-5 ">
+        <div class="services-div col-xxl-3 col-lg-4 col-md-4 col-11">
+          <div class="heart-at-bottom">
+          <h1>1</h1>
+          <ul>
+            <li><span><i class="far fa-dot-circle"></i></span>Logo</li>
+            <li><span><i class="far fa-dot-circle"></i></span>Buisness Card</li>
+            <li><span><i class="far fa-dot-circle"></i></span>Logo</li>
+            <li><span><i class="far fa-dot-circle"></i></span>Logo</li>
+          </ul>
+
+          </div>
+       
+         <i class="far fa-heart fa-4x"></i> 
+      
+     </div>
+
+      <div class="services-div col-xxl-3 col-lg-4 col-md-4 col-11">
+          <div class="heart-at-bottom">
+          <h1>2</h1>
+          <ul>
+            <li><span><i class="far fa-dot-circle"></i></span> Logo</li>
+            <li><span><i class="far fa-dot-circle"></i></span> Buisness Card</li>
+            <li><span><i class="far fa-dot-circle"></i></span> Logo</li>
+            <li><span><i class="far fa-dot-circle"></i></span> Logo</li>
+          </ul>
+
+          </div>
+       
+         <i class="far fa-heart fa-4x"></i> 
+      
+     </div>
+
+      <div class="services-div col-xxl-3 col-lg-4 col-md-4 col-11">
+          <div class="heart-at-bottom">
+          <h1>3</h1>
+          <ul>
+            <li><span><i class="far fa-dot-circle"></i></span>Logo</li>
+            <li><span><i class="far fa-dot-circle"></i></span>Logo</li>
+            <li><span><i class="far fa-dot-circle"></i></span>Logo</li>
+            <li><span><i class="far fa-dot-circle"></i></span>Logo</li>
+          </ul>
+
+          </div>
+       
+         <i class="far fa-heart fa-4x"></i> 
+      
+     </div>
+   </div>
+ </div>
+</div>
+</div>
+
+     <!----ourservices part--------->
+      <!----hire me part--------->
+      <div class="hireme-sec container-fluid">
+        <div class="row">
+          <div class="col-md-10 col-11 mx-auto">
+            <h1>I am available for freelance projects</h1><br/>
+            <p>you need inovative and creative design and designer</p>
+            <h3>lets create something extraordinary with you</h3>
+            <button class="hire-me"><i class="fas fa-check"></i>Hire me</button>
+            
+          </div>
+          
+        </div>
+        
+      </div>
+
+       <!----hire me part--------->
+        <!----contac part--------->
+ <div class="container-fluid contact-sec bg-light" id="about">
+
+  <div class="row">
+    <div class="col-11 col-md-10 mx-auto">
+      <div class="main-title">
+        <h1>Contact us</h1>
+        <p>Feel fee to conatact with me any time</p>
+        
+      </div>
+
+      <div class="row mt-5 gy-5 ">
+        <div class="col-md-3 col-10 conatct-img">
+         <figure> <img src="image/p6.png" class="img-fluid">
+         </figure>
+          
+        </div>
+
+  <div class="col-md-6 col-10 conatct-inner">
+   <form>
+     <div class="mb-3">
+  
+  <input type="text" class="form-control" id="exampleFormControlInput1" reruired atutocomplete="off" placeholder="name">
+</div>
+
+  <div class="mb-3">
+  
+  <input type="email" class="form-control" id="exampleFormControlInput1"reruired atutocomplete="0ff" placeholder="email">
+</div>
+
+  <div class="mb-3">
+  
+  <input type="number" class="form-control" id="exampleFormControlInput1" reruired atutocomplete="off" placeholder="mobile number">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Comment</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="type your message" rows="3"></textarea>
+</div>
+<div class="text-center">
+  <button class="hire-me" type="submit">submit</button></div>
+   </form>
+
+    
+  </div>
+  
+     
+
+   
+
+      
+   </div>
+ </div>
+</div>
+</div>
+        <!----contac part--------->
+             <!----contact links part--------->
+
+         <div class="container-fluid mt-5">
+            <div class="row">
+              <div class="col-11 col-md-10 mx-auto">
+                <div class="row justify-content-around gy-5">
+                <div class="col-lg-3 col-md-3 col-10 d-flex align-items-center flex-column justify-content-center contact-share-div">
+                  <p><i class="far  fa-2x fa-envelope " ></i></p>
+                  <br/>
+                  <p class="font-wight-bold">test@gmail.com </p>
+                  
+                </div>
+
+                 <div class="col-lg-3 col-md-3 col-10 d-flex align-items-center flex-column justify-content-center  contact-share-div">
+                  <p>
+                  <i class="fab fa-2x fa-whatsapp"></i></p>
+                  <br/>
+                  <p class="font-wight-bold">+91 0000 1111</p>
+                  
+                </div>
+
+                 <div class="col-lg-3 col-md-3 col-10 d-flex align-items-center flex-column justify-content-center contact-share-div"><p>
+                  <i class="fas fa-2x fa-map-marker"></i>
+                </p>
+                  <br/>
+                  <p class="font-wight-bold">city</p>
+                  
+                </div>
+              </div>
+                
+              </div>
+              
+            </div>
+            
+          </div>
+               <!----contact links part--------->
+
+
+        <!----social networking part--------->
+        <section class="my-5">
+          <div class="container">
+            <div class="row justify-content-center">
+
+               <div class="col-lg-1 col-2 socialsite-links">
+                <i class="fab fa-2x fa-facebook-f"></i>
+               </div>
+              <div class="col-lg-1 col-2 socialsite-links">
+                <i class="fab fa-2x fa-instagram"></i>
+               </div>
+
+                <div class="col-lg-1 col-2 socialsite-links">
+                <i class="fab fa-2x fa-dribbble"></i>
+               </div>
+
+                <div class="col-lg-1 col-2 socialsite-links">
+                <i class="fab fa-2x fa-pinterest"></i>
+               </div>
+
+                <div class="col-lg-1 col-2 socialsite-links">
+                <i class="fab fa-2x fa-behance"></i>
+               </div>
+              
+            </div>
+            
+          </div>
+          
+        </section>
+
+         <!----social networking part--------->
+           
+     
+
+           <!----footer  part--------->
+           <footer class="footer">
+             <div class="text-center ">
+              <p>Samia Rahman 2020 | All Right Reserved</p>
+               
+             </div>
+           </footer>
+
+
+             <!----footer part--------->
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- Popper.js first, then Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+  </body>
+</html>
